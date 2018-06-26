1455f6edeb83b583b500e5050f7b829e10a121d5
Alexander <mmaulwurff@gmail.com>
2018-06-26 07:23:19 +0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+name=idclever-starter
+
+rm -f $name.pk3 \
+&& \
+git log --pretty=format:"-%d %ai %s%n" > changelog.txt \
+&& \
+zip $name.pk3 \
+    *.txt \
+    *.md \
+&& \
+cp $name.pk3 weapon-menu-$(git describe --abbrev=0 --tags).pk3 \
+&& \
+gzdoom -glversion 3 -file \
+       $name.pk3 \
+       ~/Programs/Games/wads/maps/DOOMTEST.wad \
+       "$1" "$2" \
+       +map test \
+       -nomonsters
