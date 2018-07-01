24151abfc1486c33ed1e8f72966fc84ee374aab2
Alexander <mmaulwurff@gmail.com>
2018-07-01 18:42:21 +0700

    make changelog pretty

--- build.sh
+++ build.sh
@@ -4,7 +4,14 @@ name=idclever-starter
 
 rm -f $name.pk3 \
 && \
-git log --pretty=format:"-%d %ai %s%n" > changelog.txt \
+git log --date=short --pretty=format:"-%d %ad %s%n" | \
+    grep -v "^$" | \
+    sed "s/HEAD -> master, //" | \
+    sed "s/, origin\/master//" | \
+    sed "s/ (HEAD -> master)//" | \
+    sed "s/ (origin\/master)//"  |\
+    sed "s/- (tag: \(v\?[0-9.]*\))/\n\1\n-/" \
+    > changelog.txt \
 && \
 zip $name.pk3 \
     *.txt \
