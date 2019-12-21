080d23ed29645d6058e57239a15c568929de8e70
Alexander Kromm <allkromm@debian>
2019-12-21 18:00:08 +0700

    update build script

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-gzdoom=gzdoom
-#gzdoom=~/Documents/src/gzdoom-build/gzdoom
+set -e
 
 name=idclever-starter
-#iwad=~/Programs/Games/wads/doom/DOOM.WAD
-#iwad=~/Programs/Games/wads/doom/HERETIC.WAD
-#iwad=~/Programs/Games/wads/modules/game/chex3.wad
-#iwad=~/Programs/Games/wads/doom/HEXEN.WAD
-#iwad=~/Programs/Games/wads/doom/freedoom1.wad
-#iwad=~/Programs/Games/wads/modules/game/square1.pk3
-#iwad=~/Programs/Games/wads/modules/game/harm1.wad
 
-rm -f $name.pk3 \
-&& \
+rm -f $name.pk3
+
 git log --date=short --pretty=format:"-%d %ad %s%n" | \
     grep -v "^$" | \
     sed "s/HEAD -> master, //" | \
@@ -21,21 +13,16 @@ git log --date=short --pretty=format:"-%d %ad %s%n" | \
     sed "s/ (HEAD -> master)//" | \
     sed "s/ (origin\/master)//"  |\
     sed "s/- (tag: \(v\?[0-9.]*\))/\n\1\n-/" \
-    > changelog.txt \
-&& \
+    > changelog.txt
+
 zip $name.pk3 \
     filter/*/*.enu \
     zscript/*.zs \
     *.txt \
     *.zs \
     *.md \
-    language.enu \
-&& \
-cp $name.pk3 $name-$(git describe --abbrev=0 --tags).pk3 \
-&& \
-$gzdoom -iwad $iwad \
-        -file $name.pk3 "$1" "$2" \
-        ~/Programs/Games/wads/maps/DOOMTEST.wad \
-        +map test \
-        #+openmenu m8f_is_LevelsMenu \
-        #-norun
+    language.enu
+
+cp $name.pk3 $name-$(git describe --abbrev=0 --tags).pk3
+
+gzdoom -file $name.pk3 "$@"
